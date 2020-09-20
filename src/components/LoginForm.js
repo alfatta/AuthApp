451ae1050fd729c380867793f0d1b2fdf2f9e32f
alfatta <arwinnersyah@gmail.com>
2020-09-20 11:34:18 +0700

    Simplify common component import

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,11 +2,7 @@ import React, { Component } from 'react';
 import firebase from 'firebase';
 import { Text } from 'react-native';
 
-import Card from './Card';
-import CardSection from './CardSection';
-import Button from './Button';
-import Input from './Input';
-import Spinner from './Spinner';
+import { Card, CardSection, Button, Spinner, Input } from './common';
 
 class LoginForm extends Component {
   state = {
