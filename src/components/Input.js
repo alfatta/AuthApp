a70e1b743e9cf5ba6240914d842a6c0400358352
alfatta <arwinnersyah@gmail.com>
2020-09-20 10:23:36 +0700

    Styling input component

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react';
-import { TextInput, View, Text } from 'react-native';
+import { TextInput, View, Text, StyleSheet } from 'react-native';
+
+const style = StyleSheet.create({
+  container: {
+    height: 50,
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: "center",
+    backgroundColor: 'white',
+    borderBottomColor: '#007aff',
+    borderBottomWidth: 1
+  },
+  label: {
+    flex: 1,
+    fontSize: 18,
+    paddingLeft: 20
+  },
+  input: {
+    flex: 2,
+    paddingHorizontal: 5,
+    fontSize: 18,
+    lineHeight: 23,
+    height: 50
+  }
+})
 
 class Input extends Component {
   render() {
     const { label, placeholder, secureTextEntry } = this.props
     return (
-      <View>
-        <Text>{ label }</Text>
+      <View style={ style.container }>
+        <Text style={ style.label }>{ label }</Text>
         <TextInput
+          style={ style.input }
           placeholder={ placeholder }
           secureTextEntry={ secureTextEntry }
           autoCorrect={ false } />
